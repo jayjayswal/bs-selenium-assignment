e3bf3793a4c5dc3ca7e83c68382c354f4bc8937b
Jay Jayswal <jay@Jays-MacBook-Pro.local>
2021-07-08 18:55:31 +0530

    browserstack flag added

--- test.sh
+++ test.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-SELENIUM_SERVER_PATH="http://localhost:4444"
+AUTOMATE_USERNAME="jayjayswal_D9npvR"
+AUTOMATE_ACCESS_KEY="s9UoshQgmUN79dshsrLJ"
+
+if [ -n "$1" ] && [ $1 == '--browserstack' ]; then
+    SELENIUM_SERVER_PATH="https://${AUTOMATE_USERNAME}:${AUTOMATE_ACCESS_KEY}@hub-cloud.browserstack.com"
+    SELENIUM_CAPABILITIES="{
+        \"desiredCapabilities\": {
+            \"browserName\" : \"chrome\",
+            \"os_version\" : \"Sierra\",
+            \"resolution\" : \"1920x1080\",
+            \"browser_version\" : \"65.0\",
+            \"os\" : \"OS X\",
+            \"name\" : \"Bstack - Selenium assignment phase 2\",
+            \"build\" : \"Bstack Selenium assignment build 1\"
+        }
+    }"
+    ELEMENT_ID_KEY="ELEMENT"
+    SEARCH_BUTTON_X_PATH="/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[2]/center/input[1]"
+else
+    SELENIUM_SERVER_PATH="http://localhost:4444"
+    SELENIUM_CAPABILITIES="{
+        \"desiredCapabilities\": {
+            \"browserName\" : \"chrome\"
+        }
+    }"
+    ELEMENT_ID_KEY="element-6066-11e4-a52e-4f735466cecf"
+    SEARCH_BUTTON_X_PATH="/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"
+fi
 
 CheckLastCommandStatus() {
     retVal=$?
@@ -27,17 +54,13 @@ CheckPrerequisites
 # starting a session
 RES=$(curl -sS --location --request POST $SELENIUM_SERVER_PATH'/wd/hub/session' \
     --header 'Content-Type: application/json' \
-    --data-raw '{
-        "desiredCapabilities": {
-            "browserName" : "chrome"
-        }
-    }')
+    --data-raw "$SELENIUM_CAPABILITIES"
+    )
 CheckLastCommandStatus "Selenium failed to start session"
 SELENIUM_SESSION_ID=$(echo $RES | jq -r '.sessionId')
 # SELENIUM_SESSION_ID="c26992c29c2e81580e6d72f59ab84bb1"
 CheckLastCommandStatus "Selenium session id not found"
 echo "Selenium session ID:" $SELENIUM_SESSION_ID
-
 #opning google in
 RES=$(curl -sS --location --request POST $SELENIUM_SERVER_PATH'/wd/hub/session/'$SELENIUM_SESSION_ID'/url' \
 --header 'Content-Type: application/json' \
@@ -54,7 +77,7 @@ RES=$(curl -sS --location --request POST $SELENIUM_SERVER_PATH'/wd/hub/session/'
     "value": "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
 }')
 CheckLastCommandStatus "Error in finding input element"
-GOOGLE_TEXT_INPUT_ID=$(echo $RES | jq -r '.value | .["element-6066-11e4-a52e-4f735466cecf"]')
+GOOGLE_TEXT_INPUT_ID=$(echo $RES | jq -r '.value | .["'$ELEMENT_ID_KEY'"]')
 CheckLastCommandStatus "Error in finding input element id"
 echo "google text input ID:" $GOOGLE_TEXT_INPUT_ID
 
@@ -63,10 +86,10 @@ RES=$(curl -sS --location --request POST $SELENIUM_SERVER_PATH'/wd/hub/session/'
 --header 'Content-Type: application/json' \
 --data-raw '{
     "using" : "xpath",
-    "value": "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"
+    "value": "'$SEARCH_BUTTON_X_PATH'"
 }')
 CheckLastCommandStatus "Error in finding submit button element"
-GOOGLE_SUBMIT_BUTTON_ID=$(echo $RES | jq -r '.value | .["element-6066-11e4-a52e-4f735466cecf"]')
+GOOGLE_SUBMIT_BUTTON_ID=$(echo $RES | jq -r '.value | .["'$ELEMENT_ID_KEY'"]')
 CheckLastCommandStatus "Error in finding submit button element id"
 echo "google submit button ID:" $GOOGLE_SUBMIT_BUTTON_ID
 
